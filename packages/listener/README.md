c16685bf5ab6de8e807efe91aba703d5237c1912
Davide Perozzi <myself@davideperozzi.com>
2022-12-23 00:18:41 +0100

    feat: added custom build script

---
+++ packages/listener/README.md
@@ -0,0 +1,2 @@
+# @smoovy/listener
+[![Version](https://flat.badgen.net/npm/v/@smoovy/listener)](https://www.npmjs.com/package/@smoovy/listener) ![Size](https://flat.badgen.net/bundlephobia/minzip/@smoovy/listener)
---
+++ scripts/build.ts
@@ -0,0 +1,65 @@
+import esbuild from 'esbuild';
+import fg from 'fast-glob';
+import { execSync } from 'node:child_process';
+import fs from 'node:fs';
+import path from 'node:path';
+
+// transpile js to ts
+try {
+  const files = fg.sync(['./src/**/*.ts'], { absolute: true });
+  const output = execSync(
+    `yarn run -T esbuild ${files.join(' ')} --outdir=./dist`
+  );
+
+  console.log(output.toString('utf-8'));
+} catch(err) {
+  console.error((err as any).toString());
+  process.exit(1);
+}
+
+
+// generate declerations
+if (fs.existsSync('./tsconfig.tsbuildinfo')) {
+  fs.rmSync('./tsconfig.tsbuildinfo')
+}
+
+try {
+  execSync(`yarn run -T tsc`);
+} catch(err) {
+  const error = err as any;
+
+  if (error.output && Array.isArray(error.output)) {
+    const lines = error.output.filter((line: any) => !!line);
+    const output = lines.map((line: any) => line.toString('utf-8')).join('\n');
+
+    console.error(output);
+    process.exit(1);
+  }
+}
+
+// move general files like README.md etc.
+const copyFiles = ['README.md'];
+
+copyFiles.forEach(file => {
+  if (fs.existsSync(file)) {
+    fs.copyFileSync(file, `dist/${file}`);
+  }
+});
+
+// move package json with slight modifications
+const packageJson = (await import(
+  path.join(process.cwd(), 'package.json'),
+  { assert: { type: "json" } }
+)).default;
+
+if (packageJson.main) {
+  packageJson.main = packageJson.main
+    .replace(/^\.\/src\//gi, './')
+    .replace(/.ts$/gi, '.js');
+}
+
+fs.writeFileSync(
+  path.join(process.cwd(), 'dist/package.json'),
+  JSON.stringify(packageJson, undefined, 2)
+);
+
